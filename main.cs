82547009182b6b1dff3ccb5a0b550186fc191892
Mark-Palfi <99292598+Mark-Palfi@users.noreply.github.com>
2022-04-13 16:12:55 +0000

    code

--- main.cs
+++ main.cs
@@ -1,10 +1,15 @@
+// Created by: Mark-Palfi
+// Created on Mar 2022
+//
+// This program displays, Hello, World!
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-    
-      
-    Console.WriteLine ("Done");
-  }
+class Program 
+{
+    public static void Main (string[] args) 
+    {
+        Console.WriteLine ("Hello, World!");
+        Console.WriteLine ("\nDone.");
+    }
 }
